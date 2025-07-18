7c312b5a805ea13a46b172908d6935b039afc238
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-07-18 14:14:15 +0530

    Average of two number

---
+++ averageOftwo.py
@@ -0,0 +1,6 @@
+# Write a python program to find an average of two numbers entered by the user.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print((a + b) / 2)
