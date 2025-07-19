bdd2dc64c2fcd2267fe0b30a1ea235c2984044ac
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-07-19 14:05:12 +0530

    Tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,10 @@
+# TUPLES - A built in datatype that allows us to create 
+# immutable(that cannot be changed) sequences of values.
+
+tup = (1, 2, 3, 4)
+print(tup)
+print(type(tup))
+print(tup[2])
+
+# any particular index value in tuple can be accessed 
+# but it cannot be modified just like in string.
