65f4b00706dc22a3f1543ef14019b325ec716ee0
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-07-19 14:00:29 +0530

    String slicing

---
+++ Strings/string_slicing.py
@@ -0,0 +1,16 @@
+# SLICING - Accessing part of a string
+
+# str [starting-idx : ending-idx ] #ending idx is not included
+
+str = "Hello World"
+print(str[1:6])
+
+# Agr isme starting index nhi denge to it will assume 0
+# and ending index nhi denge to it will assume length of string
+
+# Negative Indexing 
+
+str = "Apple"
+#   -5-4-3-2-1
+print(str[-3:-1])
+# output pl
