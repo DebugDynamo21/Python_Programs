b9a2091b93a22cf5afec23ed48870f4d56c93814
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-07-19 14:02:27 +0530

    String Question

---
+++ Strings/str_Q1.py
@@ -0,0 +1,4 @@
+# WAP to input user's first name & print its length.
+
+str1 = str(input("Enter First name: "))
+print(len(str1))
---
+++ Strings/str_Q2.py
@@ -0,0 +1,4 @@
+# WAP to find the occurrence of '$' in a String.
+
+str = str(input("Enter a string:"))
+print(str.count("$"))
