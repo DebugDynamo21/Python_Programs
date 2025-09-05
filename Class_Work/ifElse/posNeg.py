8d3153dee19090b500c59b4fe04fe395964a14da
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-09-05 14:32:34 +0530

    Poaitive or Negative

---
+++ Class_Work/ifElse/posNeg.py
@@ -0,0 +1,10 @@
+# Any integer is input through the keyboard. Write a program
+# to find out whether it is a negative number or positive
+# number.
+
+num = int(input("Enter a number: "))
+
+if(num >= 0):
+    print("Positive Number")
+else:
+    print("Negative Number")
