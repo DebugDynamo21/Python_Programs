67afc8a9447a0f70674e10d62001a00746bb9d4c
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-09-20 23:49:33 +0530

    While loop implementation

---
+++ Class_Work/Loop/while.py
@@ -0,0 +1,10 @@
+# Write a program to implement while loop with else statement.
+
+n = 0
+while n < 10:
+    print(n)
+    n += 1
+else:
+    print("All items get printed")
+
+#Output - 0 1 2 3 4 5 6 7 8 9 All items get printed
