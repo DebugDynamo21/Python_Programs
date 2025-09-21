dc7124753e476550a56fb10aa7ab4d612b087ba7
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-09-21 19:24:09 +0530

    Print prime number from a to b

---
+++ Class_Work/Loop/primeNum2.py
@@ -0,0 +1,14 @@
+# Write a Python program to print all Prime numbers in given Interval.
+
+lower = int(input("Enter lower range number: "))
+upper = int(input("Ente upper range number: "))
+
+is_prime = True
+
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if(num % i) == 0:
+                break
+        else:
+            print(num)
