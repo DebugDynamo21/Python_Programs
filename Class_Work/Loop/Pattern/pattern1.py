1ce5f4250e33c61b0f45ab741fc0cee71271830a
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-09-25 19:27:15 +0530

    Square star pattern

---
+++ Class_Work/Loop/Pattern/pattern1.py
@@ -0,0 +1,13 @@
+# Python Program to print following Pattern.
+
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+n = int(input("Enter number of rows: "))
+for i in range(1,n+1):
+    for j in range (1, n+1):
+        print("*", end=" ")
+    print()
