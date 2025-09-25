cea54f8ccde3ed5c34e294822da1b5bf5e3927af
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-09-25 19:30:08 +0530

    Check a num is perfect number or not

---
+++ Class_Work/Loop/perfectNum.py
@@ -0,0 +1,12 @@
+# Python program to check whether the given number is perfect or not.
+
+num = int(input("Enter a number: "))
+sum = 0
+for i in range(1, num//2 + 1):
+    if num % i == 0:
+        sum += i
+
+if sum == num:
+    print(f"{num} is a perfect number.")
+else:
+    print(f"{num} is not a perfect number.")
