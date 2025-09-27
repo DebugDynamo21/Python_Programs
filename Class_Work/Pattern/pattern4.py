677b52dbb1bed568b6f03c8d75602da6a816e763
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-09-27 13:13:59 +0530

    Right triangle pattern

---
+++ Class_Work/Pattern/pattern4.py
@@ -0,0 +1,14 @@
+# Python Program to print following Pattern.
+
+#     *
+#     * *
+#     * * *
+#     * * * *
+#     * * * * *
+
+n = int(input("Enter number of rows: "))
+for i in range (n):
+    for j in range(n):
+        if j<=i:
+            print("*", end=" ")
+    print()
