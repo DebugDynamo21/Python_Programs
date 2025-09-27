f5a2187a52250164261bc5e7bd605b547455216c
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-09-27 13:20:43 +0530

    Right triangle pattern(number)

---
+++ Class_Work/Pattern/pattern5.py
@@ -0,0 +1,15 @@
+# Python Program to print following Pattern.
+
+#   1
+#   1 2
+#   1 2 3
+#   1 2 3 4
+#   1 2 3 4 5
+
+n = int(input("Enter number of rows: "))
+
+for i in range(1, n + 1):
+    for j in range (1, n+1):
+        if j <= i:
+            print(j, end = " ")
+    print()
