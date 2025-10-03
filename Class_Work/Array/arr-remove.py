9a84d9842aa4113177175adda642fed766e5ec77
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-10-03 19:53:21 +0530

    Remove element from array

---
+++ Class_Work/Array/arr-remove.py
@@ -0,0 +1,8 @@
+# Delete an element from an array using remove()
+
+import array as arr
+a=arr.array('i',[5,10,20,30,40])
+print("Array initially: ", a)
+
+a.remove(5)
+print("Array after remove: ", a)
