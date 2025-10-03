39d42e7fb62bf2c7b45f31842f060c5672f3de91
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-10-03 20:07:01 +0530

    Creation of list

---
+++ Class_Work/List/creation.py
@@ -0,0 +1,16 @@
+# List creation - using square brackets.
+
+list1 = ["apple", "banana", "cherry"]
+print("List 1: ", list1)
+
+# List allows duplicate values.
+list2 = ["apple", "banana", "cherry", "apple", "cherry"]
+print("List 2: ", list2)
+
+# Calculate list length.
+print("Length of List 1: ", len(list1))
+print("Length of List 2: ", len(list2))
+
+# List with mixed data types.
+list3 = ["abc", 34, True, 40, "male"]
+print("List 3: ", list3)
