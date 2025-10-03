1fc46515f2435d040f072a2d3b2b70a3514a3a89
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-10-03 20:10:01 +0530

    Create a list using list constructor

---
+++ Class_Work/List/creation2.py
@@ -0,0 +1,8 @@
+# Create a List - using list constructor.
+
+list1 = list(("apple", "banana", "cherry")) # note the double round-brackets
+print("List 1: ", list1)
+
+# List with mixed values
+list2 = list(("Hardik", True, 3.14, 'a', 23))
+print("List 2: ", list2)
