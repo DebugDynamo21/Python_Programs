33aa7c22f458195eab91d9653869f6d1eb94836e
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-10-08 11:05:33 +0530

    Finding index of element

---
+++ Class_Work/List/findIndex.py
@@ -0,0 +1,6 @@
+# Write a Python program to find the index of an element in a list.
+
+l = ['a', 2.34, 5, "hello", True, 'world', 3.24]
+print("The list is: ", l)
+
+print("The index of 'a' is: ", l.index['a'])
