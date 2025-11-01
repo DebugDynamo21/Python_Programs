0ad9083e2ee28b18d98860d697471b0d544dfab1
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-11-01 19:38:36 +0530

    Find common elements in two list

--- Class_Work/List/findCommon.py
+++ Class_Work/List/findCommon.py
@@ -3,6 +3,9 @@
 l1 = [2, 4, 6, 8, 10, 12, 14, 16, 18]
 l2 = [3, 6, 9, 12, 15, 18, 21, 24]
 
+print("List 1: ", l1)
+print("List 2: ", l2)
+
 for i in l1:
     if i in l2:
-        print(i)
+        print("Common elements from both list: "i)
