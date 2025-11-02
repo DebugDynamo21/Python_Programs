c6b90cd612c61bc1c5ffcda7bf7579839749e496
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-11-02 14:48:18 +0530

    Sum of elements at odd index

---
+++ Class_Work/List/sumOfOddIndex.py
@@ -0,0 +1,11 @@
+# Write a Python program to find the sum of elements at
+# odd indices in a list.
+
+mylist = [100,4,200,1,3,2,9,11,23,35,12,10,15,13,16,14,0]
+print("List: ", mylist)
+
+odd = mylist[1::2]
+print("List of odd elements: ", odd)
+
+t = sum(odd)
+print("Sum of odd elements from list: ", t)
