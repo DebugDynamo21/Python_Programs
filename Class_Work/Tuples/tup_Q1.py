a430969a8fbc63d44f64a6a25fc21ba0b8680877
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-11-02 15:08:14 +0530

    Addes Tuples folder to Class Work

---
+++ Class_Work/Tuples/tup_Q1.py
@@ -0,0 +1,13 @@
+# WAP to count the number of students with the "A" grade in the following tuple.
+# ["C", "D", "A", "A", "B", "B", "A"]
+
+tup = ("C", "D", "A", "A", "B", "B", "A")
+count = tup.count("A")
+
+print(count)
+
+# Store the above values in a list & sort them from "A" to "D".
+
+list = ["C", "D", "A", "A", "B", "B", "A"]
+list.sort()
+print(list)
