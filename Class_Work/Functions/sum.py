f3c282ba7db1e56135ef147e7029a3be994795cb
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-11-09 23:34:49 +0530

    Function to find sum

---
+++ Class_Work/Functions/sum.py
@@ -0,0 +1,8 @@
+def sum(a,b,c):
+    """ this function provides the sum of three numbers""" # Docstring
+    
+    s=(a+b+c)
+    return(s)
+
+a, b, c = 5, 12, 7
+print("The sum is: ", sum(a, b, c))
